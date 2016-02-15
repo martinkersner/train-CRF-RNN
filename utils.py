e1ffb60f2249afcc659492660def02ff216bb89e
Martin Keršner <m.kersner@gmail.com>
2016-02-15 10:45:13 +0900

    removed spaces from class names

--- utils.py
+++ utils.py
@@ -6,9 +6,9 @@ import numpy as np
 
 def pascal_classes():
   classes = {'aeroplane' : 1,  'bicycle'   : 2,  'bird'        : 3,  'boat'         : 4, 
-             'bottle'    : 5,  'bus'       : 6,  'car '        : 7,  'cat'          : 8, 
+             'bottle'    : 5,  'bus'       : 6,  'car'         : 7,  'cat'          : 8, 
              'chair'     : 9,  'cow'       : 10, 'diningtable' : 11, 'dog'          : 12, 
-             'horse'     : 13, 'motorbike' : 14, 'person'      : 15, 'potted plant' : 16, 
+             'horse'     : 13, 'motorbike' : 14, 'person'      : 15, 'potted-plant' : 16, 
              'sheep'     : 17, 'sofa'      : 18, 'train'       : 19, 'tv/monitor'   : 20}
 
   return classes
